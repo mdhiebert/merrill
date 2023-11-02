6a77c27851a73363e8127b4ef0ae7645d59ecd54
Jacob White <jacobswhite@me.com>
2023-11-01 21:58:28 -0400

    Web Update - 0.0.3

--- web/controller.py
+++ web/controller.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from templates.formatter import TxtOutput
+from os.path import exists
+from templates.formatter import txtHeader, txtBody
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
+
+
 
 class testModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,17 +20,22 @@ class testModel(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
     
-class equipmentBii(db.Model):
+class equipmentTemp(db.Model):
 
     UUID = db.Column(db.Integer, primary_key=True, nullable=False)
-    SERIAL_NUMBER = db.Column(db.String(50))
+    SERIAL_NUMBER = db.Column(db.String(500))
     ITEM_DESCRIPTION = db.Column(db.String(255))
     ASSOCIATED_END_ITEM = db.Column(db.String(255))
     ITEM_QUANTITY = db.Column(db.Integer)
     DATE_CREATED = db.Column(db.DateTime, default=datetime.utcnow)
+    BOX_NUMBER = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<Task %r>' % self.id    
+        return '{}, {}, {}'.format(self.ITEM_QUANTITY, self.ITEM_DESCRIPTION, self.SERIAL_NUMBER)
+
+if exists('/instance/test.db') == False:
+    with app.app_context():
+        db.create_all()
 
 class adminClass:
     admin_to = 'A'
@@ -34,24 +44,25 @@ class adminClass:
 
 currentAdmin = adminClass()
 itemsEntered = {}
-testDict = {}
 
 @app.context_processor
 def inject_admin():
     return dict(currentAdmin=currentAdmin)
+@app.context_processor
 def inject_items():
     return dict(itemsEntered=itemsEntered)
 
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    showEquipment = equipmentBii.query.order_by(equipmentBii.SERIAL_NUMBER).all()
-    return render_template('index.html', itemsEntered=itemsEntered)
+    showEquipment = equipmentTemp.query.order_by(equipmentTemp.UUID).all()
+    return render_template('index.html', showEquipment=showEquipment)
 
 @app.route('/add_equipment' , methods=['POST', 'GET'])
 def add_equipment():
     if request.method == 'POST':
         form_data = request.form['content']
-        new_item = equipmentBii(SERIAL_NUMBER=form_data)
+        new_item = equipmentTemp(SERIAL_NUMBER=form_data)
         try:
             db.session.add(new_item)
             db.session.commit()
@@ -59,8 +70,7 @@ def add_equipment():
         except:
             return 'ERROR'
     else:
-        showEquipment = equipmentBii.query.order_by(equipmentBii.SERIAL_NUMBER).all()
-        return render_template('index.html', showEquipment=showEquipment)
+        return render_template('index.html')
     
 @app.route('/update_admin' , methods=['POST', 'GET'])
 def update_admin():
@@ -70,20 +80,59 @@ def update_admin():
         newItems.pop('to')
         newItems.pop('from')
         newItems.pop('packed_by')
+        allDB = equipmentTemp.query.order_by(equipmentTemp.UUID).all()
+        dbuuids = []
+        for uuid in allDB:
+            dbuuids.append(uuid.UUID)
+        if 'UUID' in newItems:
+            uuids = newItems['UUID']            
+            print(len(dbuuids))
+            for x in range(len(uuids)):
+                uuids[x] = int(uuids[x])
+            for y in dbuuids:
+                if y not in uuids:
+                    del_item = equipmentTemp.query.filter_by(UUID=y).first()
+                    print(del_item)
+                    try:
+                        db.session.delete(del_item)
+                        db.session.commit()
+                    except:
+                        return 'Error'
+            newItems.pop('UUID')
+        elif len(dbuuids) > 0:
+            for x in dbuuids:
+                del_item = equipmentTemp.query.filter_by(UUID=x).first()
+            try:
+                db.session.delete(del_item)
+                db.session.commit()
+            except:
+                return 'Error'
+        tmpItems = newItems.copy()
+        curItems = newItems.copy()
+        for x in newItems.keys():
+            if 'new' in x:
+                curItems.pop(x)
+            else: 
+                tmpItems.pop(x)
         ctr = 1
         lst = []
-        for x,y in newItems.items():
+        for x,y in tmpItems.items():
             lst.append(str(y[0]))
-            if ctr == 3:
+            if ctr == 4:
                 if lst[0] == ''  and (lst[1] == '' and lst[2] == ''):
                     lst = []
                     ctr = 1
                     continue                    
                 newLen = len(itemsEntered) + 1
                 itemsEntered[newLen]=lst
+                new_item = equipmentTemp(SERIAL_NUMBER=lst[2], ITEM_DESCRIPTION=lst[1], ITEM_QUANTITY=lst[0], BOX_NUMBER=lst[3])
+                try:
+                    db.session.add(new_item)
+                    db.session.commit()
+                except:
+                    return 'Error'
                 lst = []
                 ctr = 1
-                print(itemsEntered)
                 continue  
             ctr = ctr + 1
         if request.form['to'] != '':
@@ -97,13 +146,33 @@ def update_admin():
         except:
             return 'ERROR'
     else:
-        showEquipment = equipmentBii.query.order_by(equipmentBii.SERIAL_NUMBER).all()
-        return render_template('index.html', showEquipment=showEquipment)
+        return render_template('index.html')
 
 @app.route('/generate_txt' , methods=['POST', 'GET'])
 def generate_txt():
-    o = TxtOutput('test', currentAdmin.admin_from, currentAdmin.admin_to, currentAdmin.admin_packedby, itemsEntered)
-    o.fromForm()
+    allDB = equipmentTemp.query.order_by(equipmentTemp.BOX_NUMBER, equipmentTemp.UUID).all()
+    header = txtHeader('test', currentAdmin.admin_from, currentAdmin.admin_to, currentAdmin.admin_packedby)
+    outDB = {}
+    for x in allDB:
+        if x.BOX_NUMBER not in outDB:
+            content = {x.UUID: [x.ITEM_QUANTITY, x.SERIAL_NUMBER, x.ITEM_DESCRIPTION]}
+            outDB.update({x.BOX_NUMBER:content})
+        else:
+            content = outDB[x.BOX_NUMBER]
+            ncon = [x.ITEM_QUANTITY, x.SERIAL_NUMBER, x.ITEM_DESCRIPTION]
+            content[x.UUID] = ncon
+            outDB.update({x.BOX_NUMBER:content})
+
+    print(outDB)
+    textContent = txtBody(outDB)    
+    output = header + textContent
+    with open('test_output/testoutput.txt', 'w+') as f:
+        f.write('{}'.format(output))
+        f.close
+    return redirect('/')
+
+@app.route('/delete_row', methods=['POST', 'GET'] )
+def delete_row():
     return redirect('/')
 
 if __name__ == '__main__':
--- web/templates/formatter.py
+++ web/templates/formatter.py
@@ -1,31 +1,49 @@
-class TxtOutput:
+""" class TxtOutput:
 
-    def __init__(self, overall_description:str, admin_from:str, admin_to:str, packed_by:str, items ):
+    def __init__(self, overall_description:str, admin_from:str, admin_to:str, packed_by:str):
         self.overall_description = overall_description
         self.admin_from = admin_from
         self.admin_to = admin_to
-        self.packed_by = packed_by
-        self.items = items
+        self.packed_by = packed_by """
 
-    def fromForm(self):
+""" def fromForm(self):
 
-        output = \
-            'Admin Data:\n' \
-            + 'OVERALL_DESCRIPTION: ' + self.overall_description + '\n' \
-            + '2062_FROM: ' + self.admin_from + '\n' \
-            + '2062_TO: ' + self.admin_to + '\n' \
-            + '1750_PACKED_BY: ' + self.packed_by + '\n' \
-            + '--------------------------------------------------------------------------------\n'
+    output = \
+        'Admin Data:\n' \
+        + 'OVERALL_DESCRIPTION: ' + self.overall_description + '\n' \
+        + '2062_FROM: ' + self.admin_from + '\n' \
+        + '2062_TO: ' + self.admin_to + '\n' \
+        + '1750_PACKED_BY: ' + self.packed_by + '\n' \
+        + '--------------------------------------------------------------------------------\n'
         
-        for x in self.items:
-            i = self.items[x]
-            q = i[0]
-            d = i[1]
-            s = i[2]
-            output = output + \
-                q + 'x ' + d + ' (' + s + ')\n'
+    allDB = equipmentTemp.query.order_by(equipmentTemp.UUID).all()
+    print(allDB)
             
-        with open('test_output/testoutput.txt', 'w+') as f:
-            f.write('{}'.format(output))
-            f.close
-        
+    with open('test_output/testoutput.txt', 'w+') as f:
+        f.write('{}'.format(output))
+        f.close
+
+        def fromForm(self): """
+
+def txtHeader(overall_description:str, admin_from:str, admin_to:str, packed_by:str):
+    output = \
+        'Admin Data:\n' \
+        + 'OVERALL_DESCRIPTION: ' + overall_description + '\n' \
+        + '2062_FROM: ' + admin_from + '\n' \
+        + '2062_TO: ' + admin_to + '\n' \
+        + '1750_PACKED_BY: ' + packed_by + '\n' \
+        + '--------------------------------------------------------------------------------\n'
+    return output
+
+def txtBody(bodyInput):
+    output = ''
+    for x,y in bodyInput.items():
+        for f in y.values():
+            output = output + \
+                str(f[0]) + 'x ' + \
+                str(f[2]) + ' (' + \
+                str(f[1]) + ')\n'
+        if x != list(bodyInput.keys())[-1]: output = output + '#\n'
+    return output
+
+            
